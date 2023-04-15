49a2696908b9ddda99cdca48d9d67eafa235b55c
Daniel Albl <daniel.albl@vivint.com>
2023-04-15 16:44:09 -0600

    Scale A and B at the end

--- pythagorean/main.py
+++ pythagorean/main.py
@@ -17,9 +17,14 @@ MARGIN = (B - A) / 2
 class PythagoreanTheorem(Scene):
     @staticmethod
     def create_triangle(a, b, c):
-        return Polygon(a, b, c, stroke_width=STROKE_WIDTH, color=WHITE)
+        return VGroup(
+            Line(a, b, stroke_width=STROKE_WIDTH, color=A_COLOR),
+            Line(b, c, stroke_width=STROKE_WIDTH, color=B_COLOR),
+            Line(c, a, stroke_width=STROKE_WIDTH, color=C_COLOR)
+        )
 
-    def create_square(self, side_length, color):
+    @staticmethod
+    def create_square(side_length, color):
         return VGroup(
             Line(side_length * UL / 2, side_length * UR / 2, stroke_width=STROKE_WIDTH, color=color),
             Line(side_length * UR / 2, side_length * DR / 2,  stroke_width=STROKE_WIDTH, color=color),
@@ -37,12 +42,7 @@ class PythagoreanTheorem(Scene):
         self.b.move_to((A + B) / 2 * DOWN)
         self.c.move_to([np.cos(TH) * C / 2, np.sin(TH) * C / 2, 0])
 
-        self.t = VGroup(
-            Line(self.a.get_corner(UR), self.a.get_corner(DR), stroke_width=STROKE_WIDTH, color=A_COLOR),
-            Line(self.a.get_corner(DR), self.b.get_corner(UR), stroke_width=STROKE_WIDTH, color=B_COLOR),
-            Line(self.b.get_corner(UR), self.a.get_corner(UR), stroke_width=STROKE_WIDTH, color=C_COLOR),
-            stroke_width=STROKE_WIDTH,
-        )
+        self.t = self.create_triangle(self.a.get_corner(UR), self.a.get_corner(DR), self.b.get_corner(UR))
 
         self.play(AnimationGroup(
             FadeIn(self.a, run_time=RUN_TIME),
@@ -98,8 +98,44 @@ class PythagoreanTheorem(Scene):
             ApplyMethod(self.r4.shift, (SHIFT + A) * RIGHT + B * UP)
         ))
 
+    def change_a_and_b(self, d):
+        global A, B, C, TH
+        _a = deepcopy(self.a).scale(1 + d / A, about_point=self.a.get_corner(DL))
+        _b = deepcopy(self.b).scale(1 - d / B, about_point=self.b.get_corner(UR))
+        _th = np.arctan((B - d) / (A + d))
+        _c = deepcopy(self.c).rotate(_th - TH).scale(np.sqrt((A + d) ** 2 + (B - d) ** 2) / C)
+        self.play(AnimationGroup(
+            Transform(self.a, _a),
+            Transform(self.b, _b),
+            Transform(self.c, _c),
+
+            Transform(self.l1, self.create_triangle(
+                self.l1.get_corner(UL) + d * UR, self.l1.get_corner(DL) + d * RIGHT, self.l1.get_corner(DR))),
+            Transform(self.l2, self.create_triangle(
+                self.l2.get_corner(DL) + d * UP, self.l2.get_corner(DR) + d * UR, self.l2.get_corner(UR) + d * RIGHT)),
+            Transform(self.l3, self.create_triangle(
+                self.l3.get_corner(UR) + d * RIGHT, self.l3.get_corner(UL), self.l3.get_corner(DL) + d * UP)),
+            Transform(self.l4, self.create_triangle(
+                self.l4.get_corner(DR), self.l4.get_corner(UR) + d * UP, self.l4.get_corner(UL) + d * UR)),
+
+            Transform(self.r1, self.create_triangle(
+                self.r1.get_corner(UL) + d * UP, self.r1.get_corner(DL), self.r1.get_corner(DR) + d * LEFT)),
+            Transform(self.r2, self.create_triangle(
+                self.r2.get_corner(DL) + d * LEFT, self.r2.get_corner(DR), self.r2.get_corner(UR) + d * DOWN)),
+            Transform(self.r3, self.create_triangle(
+                self.r3.get_corner(UR) + d * RIGHT, self.r3.get_corner(UL), self.r3.get_corner(DL) + d * UP)),
+            Transform(self.r4, self.create_triangle(
+                self.r4.get_corner(DR) + d * DOWN, self.r4.get_corner(UR), self.r4.get_corner(UL) + d * RIGHT)),
+        ), run_time=RUN_TIME)
+
+        A, B, C, TH = A + d, B - d, np.sqrt((A + d) ** 2 + (B - d) ** 2), _th
+
     def construct(self):
         self.create_shapes()
         self.move_squares()
         self.move_triangles()
+        _A, _B = A, B
+        self.change_a_and_b(B / 2)
+        self.change_a_and_b(-A / 2)
+        self.change_a_and_b(_A - A)
         self.wait(3)
